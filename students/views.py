fe28a73186c457cc8b9e3417b0cfca69f8e8dd07
vasiliymigovich <vasiliymigovich@gmail.com>
2016-02-02 11:23:40 +0200

    changed students_list.html and group_list.html in our project

--- students/views.py
+++ students/views.py
@@ -35,7 +35,18 @@ def students_delete(request, sid):
 
 # Views for Groups
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    groups = (
+        {'id': 1,
+         'name': u'Мтм-1',
+         'leader': {'id': 1, 'name': u'Тарас Мельник'}},
+        {'id': 2,
+         'name': u'Мтм-22',
+         'leader': {'id': 2, 'name': u'Микола Садовський'}},
+    )
+    return render(request, 'students/groups_list.html',
+        {'groups': groups})
+#def groups_list(request):
+#    return HttpResponse('<h1>Groups Listing</h1>')
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
