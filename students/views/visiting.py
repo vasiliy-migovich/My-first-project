1b6459dccff053c9995ecb351b6a7986cda30893
vasiliymigovich <vasiliymigovich@gmail.com>
2016-02-06 15:38:56 +0200

    add folder view to our project

---
+++ students/views/visiting.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Views for Students
+def visiting_list(request):
+	students = (
+		{'id': 1,
+		 'first_name': u'Корост',
+		 'last_name': u'Андрій',
+		 'ticket': 2123,
+		 'image': 'img/me.jpeg'}, 
+		{'id': 2,
+		 'first_name': u'Подоба',
+		 'last_name': u'Віталій',
+		 'ticket': 254,
+		 'image': 'img/podoba.jpg'}, 
+		{'id': 3,
+		 'first_name': u'Притула',
+		 'last_name': u'Тарас',
+		 'ticket': 5132,
+		 'image': 'img/piv.png'}
+	)
+	return render(request, 'students/visiting_list.html', {'students': students})
+"""
+def students_add(request):
+    return HttpResponse('<h1>Student Add Form</h1>')
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)"""
